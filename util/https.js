b8fd9bcb5310bc5bc387fa1d68bdd86ebe2f4f06
Berke Erdem Özdemir <berdemozdemir@gmail.com>
2023-10-18 23:28:24 +0300

    all done

--- util/https.js
+++ util/https.js
@@ -2,24 +2,6 @@ const { QueryClient } = require("@tanstack/react-query");
 
 export const queryClient = new QueryClient();
 
-// export async function fetchDogs(props) {
-//   const response = await fetch(
-//     `https://api.thecatapi.com/v1/images/search?page=0&breed_ids=${props.breed}&limit=10`
-//   );
-//   // "https://api.thedogapi.com/v1/breeds?api_key=live_I1rWYTyr4pZeJJwHD35eTfHyTKk9OAEOlGwYp98MdePtEQCYIJcd8d6hOFcXLRF6&limit=10"
-//   // "https://api.thecatapi.com/v1/breeds?api_key=live_I1rWYTyr4pZeJJwHD35eTfHyTKk9OAEOlGwYp98MdePtEQCYIJcd8d6hOFcXLRF6&limit=10"
-
-//   if (!response.ok) {
-//     const error = new Error("Something went wront while fetching dogs..");
-//     error.code = response.status;
-//     error.info = await response.json();
-//     throw error;
-//   }
-//   const data = await response.json();
-
-//   return data;
-// }
-
 export async function fetchDogDetail(id) {
   const response = await fetch(`https://api.thecatapi.com/v1/images/${id}`);
 
